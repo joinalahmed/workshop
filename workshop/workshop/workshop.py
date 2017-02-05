03b3e11b384aaaa1b8dae65650dbaed5e31f9c6c
saeed bibak <saeed.bibak@live.com>
2017-02-05 22:26:23 +0330

    hello and print

--- workshop/workshop/workshop.py
+++ workshop/workshop/workshop.py
@@ -1,1 +1,23 @@
-# Just a test
+# Just printing hello world
+
+# Note that print	is a function
+print('hello world') 
+
+
+
+# Numbers
+# Two kind of numbers : 
+print(52.3E-4)
+
+print(2 + 2) 
+print(2.0 + 2)
+
+
+
+
+
+
+
+
+
+
